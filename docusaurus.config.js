2e22e94c1cb85ab96f32d8ef22062d0768d4bc23
Guillaume Lambert <guil.lambert@protonmail.com>
2022-09-19 17:31:22 -0400

    Updated text in protocol overview, added background gradient

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/palenight');
-const darkCodeTheme = require('prism-react-renderer/themes/palenight');
+const darkCodeTheme = require('prism-react-renderer/themes/shadesofpurple');
 
 const math = require('remark-math');
 const katex = require('rehype-katex');
