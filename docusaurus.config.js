e20d862ec73efd872f5be01528069c2eb7b03ee3
Guillaume Lambert <guil.lambert@protonmail.com>
2022-09-27 14:35:19 -0400

    docs: used shadesOfPurple

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/palenight');
-const darkCodeTheme = require('prism-react-renderer/themes/shadesofpurple');
+const darkCodeTheme = require('prism-react-renderer/themes/shadesOfPurple');
 
 const math = require('remark-math');
 const katex = require('rehype-katex');
