540358db269d1bfb1e4dab158f764e61cc2c4a33
BrandonL20 <ly.brandon.1000@gmail.com>
2023-03-23 13:24:52 -0600

    Added thumbnail meta tag for twitter

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -122,7 +122,14 @@ const config = {
           hideable: true,
         },
       },
-      metadata: [{property: 'og:image', content: 'img/logo.svg'}],
+      metadata: [{
+        property: 'og:image',
+        content: 'img/logo.svg'
+      },
+      {
+        name: 'twitter:image',
+        content: 'img/logo.svg'}
+      ],
     }),
 
   // Even if you don't use internalization, you can use this field to set useful
