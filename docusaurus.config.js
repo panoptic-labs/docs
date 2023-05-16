4081165cb8f80d635a5cf117ce6e952ddb064847
BrandonL20 <ly.brandon.1000@gmail.com>
2023-05-16 10:40:41 -0600

    Added search functionality

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -175,6 +175,27 @@ const config = {
       },
       image:
         "https://raw.githubusercontent.com/panoptic-labs/docs/main/static/img/logo.png",
+      algolia: {
+        appId: '8ICJTW297L',
+        // Public API key: it is safe to commit it
+        apiKey: '5274a086bb1beb9f844aa87f71435c7b',
+        indexName: 'panoptic',
+        contextualSearch: true,
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+      },
     }),
 
   // Even if you don't use internalization, you can use this field to set useful
