9a0702f36fe084cec3968f210497a3409a3422f1
BrandonL20 <ly.brandon.1000@gmail.com>
2023-06-23 22:58:15 -0600

    Updated recent updates

--- src/components/HomePage/RecentUpdates.js
+++ src/components/HomePage/RecentUpdates.js
@@ -128,60 +128,60 @@ const RecentUpdates = () => {
 
 const recentUpdates = [
   {
-    title: "Panoptic Named Honorable Mention in Fast Company’s 2023 World Changing Ideas Awards",
-    image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*CQaWP1zIXUUcOprGQItehQ.png",
+    title: "Join the Panoptic Ambassador Program",
+    image: "https://panoptic.xyz/assets/images/panoptic_ambassador_program_banner-a0700278784e3848c596c6b2ab66ad10.png",
     excerpt:
-      "Fast Company revealed the winners of their highly-anticipated 2023 World Changing Ideas Awards today. Among the remarkable entries was Panoptic, our groundbreaking, permissionless options protocol which earned an honorable mention in the crypto and blockchain category.",
-    link: "https://blog.panoptic.xyz/panoptic-named-honorable-mention-in-fast-companys-2023-world-b30092095b9a",
-    date: "2023-05-02 00:00:00",
+      "With our gated launch drawing near, we are delighted to announce the initiation of the Panoptic Ambassador Program. If you are looking to gain experience working with a cutting-edge, early-stage crypto protocol — and get compensated for it — this is your opportunity.",
+    link: "https://panoptic.xyz/blog/panoptic-ambassador-program",
+    date: "2023/06/24 00:00:00",
   },
   {
-    title: "Panoptic at Cornell Blockchain Conference 2023",
-    image: "https://miro.medium.com/v2/resize:fit:4800/format:webp/0*92UAhLAzuyok2ack.jpeg",
+    title: "Welcome to the Panoptic Community",
+    image: "https://panoptic.xyz/assets/images/panoptic_community_launch_banner-9208d531724b2682310e58eac45ee6e0.png",
     excerpt:
-      "In this post, we summarize our attendance of the 2023 Cornell Blockchain Conference which had record participation — the venue completely sold out of its 600 tickets!",
-    link: "https://blog.panoptic.xyz/panoptic-at-cornell-blockchain-conference-2023-49f47c04aa44",
-    date: "2023-04-29 21:52:10",
+      "We are thrilled to announce the launch of our brand new community. This is your invitation to be a part of the future of decentralized options trading.",
+    link: "https://panoptic.xyz/blog/panoptic-community-launch",
+    date: "2023/06/16 00:00:00",
   },
   {
-    title: "Mastering Panoptic: The Definitive Resource Guide for All Things Panoptic",
-    image: "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*zWYRNzaaogbe0t1xUj_dsg.png",
+    title: "Reality Check: Retail Prefers 2x over 125x Leverage",
+    image: "https://panoptic.xyz/assets/images/meme-fc415cdd51a1dcc647d56e8c9d0a1e49.png",
     excerpt:
-      "We are thrilled to announce the official launch of Panoptic Docs, the ultimate resource for understanding our innovative “magical” technology.",
-    link: "https://blog.panoptic.xyz/mastering-panoptic-the-definitive-resource-guide-cf6dd50c15c5",
-    date: "2023-04-04 21:52:10",
+      "In this post, we'll debunk the myth that crypto retail want max leverage.",
+    link: "https://panoptic.xyz/research/retail-prefers-2x-over-125x-leverage",
+    date: "2023/06/14 00:00:00",
   },
   {
-    title: "Panoptic Research launches on Substack",
-    image: "/img/recent-updates/post-1.png",
+    title: "Panoptic Partners with Three Sigma For Enhanced Economic Security",
+    image: "https://panoptic.xyz/assets/images/panotic_three_sigma_partnership_banner-e37b4b41efe16d815e980d3aa027406d.png",
     excerpt:
-      "The Panoptic liquidity provider (PLP) provides fungible liquidity to the Panoptic pool and receives commission fees in return.",
-    link: "https://blog.panoptic.xyz/panoptic-research-launches-on-substack-abe7473693d6",
-    date: "2023-03-20 21:52:10",
+      "To further ensure we provide comprehensive security to our state-of-the-art options protocol, Panoptic is pleased to announce a partnership with Three Sigma, an organization that specializes in agent-based simulations and modeling.",
+    link: "https://panoptic.xyz/blog/panoptic-three-sigma-partnership",
+    date: "2023/06/09 00:00:00",
   },
   {
-    title: "Panoptic Partners with OpenZeppelin for their Second Smart Contract Audit",
-    image: "/img/recent-updates/post-2.png",
+    title: "Why Options Will Overtake DeFi",
+    image: "https://panoptic.xyz/assets/images/img-1-dfa75ec7cea7df57180683b4fbf806e5.png",
     excerpt:
-      "The Panoptic liquidity provider (PLP) provides fungible liquidity to the Panoptic pool and receives commission fees in return.",
-    link: "https://blog.panoptic.xyz/panoptic-partners-with-openzeppelin-for-their-second-smart-contract-audit-379e9bd25540",
-    date: "2023-03-08 21:52:10",
+      "With the increasing trend of retail options trading in TradFi, it's only a matter of time before this trend sweeps into DeFi. Here's 8 reasons why retail options will overtake DeFi and how Panoptic will be at the center of it all.",
+    link: "https://panoptic.xyz/research/why-options-will-overtake-defi",
+    date: "2023/06/06 00:00:00",
   },
   {
-    title: "ETH Denver 2023—A Recap",
-    image: "/img/recent-updates/post-3.png",
+    title: "Democratizing DeFi Research Part III: Deep Dive into Backtesting Panoptions",
+    image: "https://panoptic.xyz/assets/images/banner-democratizing-research-part-3-backtest-f0c008c3e45f78a311424556216b57c4.jpg",
     excerpt:
-      "The Panoptic liquidity provider (PLP) provides fungible liquidity to the Panoptic pool and receives commission fees in return.",
-    link: "https://blog.panoptic.xyz/eth-denver-2023-a-recap-8b498abd9196",
-    date: "2023-03-08 21:52:10",
+      "To conclude our three-part Democratizing DeFi Research series, Brandon guides us through the Panoption backtester.",
+    link: "https://panoptic.xyz/blog/democratizing-defi-options-research-backtest",
+    date: "2023/06/02 00:00:00",
   },
   {
-    title: "Panoptic’s V1 Roadmap",
-    image: "/img/recent-updates/post-3.png",
+    title: "Democratizing DeFi Research Part II: Deep Dive into Implied Volatility",
+    image: "https://panoptic.xyz/assets/images/democratizing-defi-research-banner-2-9044f20af3c38a3d5360962fc55c6acf.jpg",
     excerpt:
-      "The Panoptic liquidity provider (PLP) provides fungible liquidity to the Panoptic pool and receives commission fees in return.",
-    link: "https://blog.panoptic.xyz/panoptics-v1-roadmap-d7b72a8e2005",
-    date: "2023-03-05 21:52:10",
+      "In this second installment of our three-part series on Democratizing DeFi Research, Doctor C gives us a deep dive into the open-source Python script used for studying implied volatility and volatility smiles of Uniswap Liquidity Providers (LPs).",
+    link: "https://panoptic.xyz/blog/democratizing-defi-options-research-implied-volatility",
+    date: "2023-05-31 00:00:00",
   },
 ];
 
