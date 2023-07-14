29829a5a552652dcb719e21bcdde8a5d40a9bc2b
Zachary <zachary.rudebeck@gmail.com>
2023-07-13 21:12:19 -0500

    changes for mobile

--- src/hooks/useResponsive.js
+++ src/hooks/useResponsive.js
@@ -1,28 +1,10 @@
-// import { useEffect, useState } from "react";
 import { useMediaQuery } from 'react-responsive'
 
 const useResponsive = () => {
-  // const [width, setWidth] = useState(1920);
-  // const isTabletWidth = width <= 1024;
-  // const isMobileWidth = width <= 767;
-  // const is440 = width <= 440;
 
   const isTabletWidth = useMediaQuery({ query: '(max-width: 1024px)' })
   const isMobileWidth = useMediaQuery({ query: '(max-width: 767px)' })
   const is440 = useMediaQuery({ query: '(max-width: 440px)' })
-
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setWidth(window.innerWidth);
-  //   };
-
-  //   handleResize();
-  //   window.addEventListener("resize", handleResize);
-
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, []);
 
   return {
     isMobileWidth,
