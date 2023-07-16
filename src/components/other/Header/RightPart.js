85fd6d4b655e5197a13a5f7810caa648470db2fc
Zachary <zachary.rudebeck@gmail.com>
2023-07-15 23:24:28 -0500

    fixed disabled button bug

--- src/components/other/Header/RightPart.js
+++ src/components/other/Header/RightPart.js
@@ -5,19 +5,17 @@ import Button from "../../NewHomePage/Button/Button";
 import useResponsive from "../../../hooks/useResponsive";
 
 const RightPart = ({ onToggle }) => {
-  const { isTabletWidth, isMobileWidth } = useResponsive();
-  const appIsDisabled = true; // Set the app disabled state
+  const { isTabletWidth } = useResponsive();
 
   return (
     <div className="right-part">
-      {!isTabletWidth && (
-        <Button disabled={appIsDisabled}>
-          {appIsDisabled ? "App Coming Soon" : "Launch App"}
-        </Button>
-      )}
-      {isTabletWidth && (
+      {isTabletWidth ? (
         <Button disabled={false} className="right-part-mobile-button" onClick={onToggle}>
           <i className="icon__burger right-part__icon" />
+        </Button>
+      ) : (
+        <Button disabled={true}>
+          {appIsDisabled ? "App Coming Soon" : "Launch App"}
         </Button>
       )}
     </div>
