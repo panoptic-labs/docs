981b2f3ffff89d7249f6e298686de8dbb7a209a2
Zachary <zachary.rudebeck@gmail.com>
2023-07-17 14:03:23 -0500

    fix for loading bugs

--- src/hooks/useResponsive.js
+++ src/hooks/useResponsive.js
@@ -1,15 +1,26 @@
-import { useMediaQuery } from 'react-responsive'
+import { useEffect, useState } from "react";
 
 const useResponsive = () => {
+  const [width, setWidth] = useState(1920);
+  const isTabletWidth = width <= 1024;
+  const isMobileWidth = width <= 767;
 
-  const isTabletWidth = useMediaQuery({ query: '(max-width: 1024px)' })
-  const isMobileWidth = useMediaQuery({ query: '(max-width: 767px)' })
-  const is440 = useMediaQuery({ query: '(max-width: 440px)' })
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return {
     isMobileWidth,
     isTabletWidth,
-    is440,
   };
 };
 
