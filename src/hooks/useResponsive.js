cac3c04c1acee010e4680ab2f1605c292cd0fa27
Zachary <zachary.rudebeck@gmail.com>
2023-07-18 09:04:30 -0500

    fix for loading bugs

--- src/hooks/useResponsive.js
+++ src/hooks/useResponsive.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const useResponsive = () => {
   const [width, setWidth] = useState(1920);
+  const [loadedWidth, setLoadedWidth] = useState(false)
   const isTabletWidth = width <= 1024;
   const isMobileWidth = width <= 767;
 
@@ -12,6 +13,7 @@ const useResponsive = () => {
 
     handleResize();
     window.addEventListener("resize", handleResize);
+    setLoadedWidth(true)
 
     return () => {
       window.removeEventListener("resize", handleResize);
@@ -21,6 +23,7 @@ const useResponsive = () => {
   return {
     isMobileWidth,
     isTabletWidth,
+    loadedWidth,
   };
 };
 
