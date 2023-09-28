4e50816d6884f94352216532f6f148e108fa4c39
Zachary <zachary.rudebeck@gmail.com>
2023-09-28 13:12:05 -0600

    Fixed broken links

--- src/components/other/Header/Nav.js
+++ src/components/other/Header/Nav.js
@@ -20,24 +20,24 @@ const Nav = ({purpleMode = false}) => {
 
   return (
     <nav className="header__nav">
-      <Link onClick={() => handleClick("/docs/intro")} className={clsx("nav__link", {"white-text": purpleMode})}>
+      <div onClick={() => handleClick("/docs/intro")} className={clsx("nav__link", {"white-text": purpleMode})}>
         Docs
-      </Link>
-      <Link onClick={() => handleClick("/blog")} className={clsx("nav__link", {"white-text": purpleMode})}>
+      </div>
+      <div onClick={() => handleClick("/blog")} className={clsx("nav__link", {"white-text": purpleMode})}>
         Blog
-      </Link>
-      <Link onClick={() => handleClick("/research")} className={clsx("nav__link", {"white-text": purpleMode})}>
+      </div>
+      <div onClick={() => handleClick("/research")} className={clsx("nav__link", {"white-text": purpleMode})}>
         Deep Dive
-      </Link>
-      <Link onClick={() => handleClick("/docs/faq/")} className={clsx("nav__link", {"white-text": purpleMode})}>
+      </div>
+      <div onClick={() => handleClick("/docs/faq/")} className={clsx("nav__link", {"white-text": purpleMode})}>
         FAQ
-      </Link>
-      <Link onClick={() => handleClick("https://github.com/panoptic-labs")} className={clsx("nav__link", {"white-text": purpleMode})}>
+      </div>
+      <div onClick={() => handleClick("https://github.com/panoptic-labs")} className={clsx("nav__link", {"white-text": purpleMode})}>
         Github
         <span className="external-link-arrow">
           <img src={iconPath} alt="arrow"/>
         </span>
-      </Link>
+      </div>
     </nav>
   );
 };
