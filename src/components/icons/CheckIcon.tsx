f353aad2ec38c254deb84bcd54197a5359ceb2fe
Thomas Thachil <github@thomasthachil.com>
2023-10-17 23:57:55 -0400

    fixes from designer

--- src/components/icons/CheckIcon.tsx
+++ src/components/icons/CheckIcon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CheckIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={30} height={30}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" width={24} height={24}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
     </svg>
   )
