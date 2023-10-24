842a583fa287a74c69280d8349dce03e9c8b5c77
Thomas Thachil <github@thomasthachil.com>
2023-10-24 17:09:13 -0400

    switch logo

--- src/components/icons/PanoptimistsIcon.tsx
+++ src/components/icons/PanoptimistsIcon.tsx
@@ -3,6 +3,6 @@ import React from "react";
 
 export default function PanoptimistsIcon() {
   return (
-    <img src="./img/icons/panoptimists.png" alt="Panoptimists Icon" />
+    <img src="./img/logo.svg" alt="Panoptimists Icon" />
   )
 }
