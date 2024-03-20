10f0f7c5df03fc9652f1c1ca684f667fad60ab14
Thomas Thachil <github@thomasthachil.com>
2024-03-19 23:16:19 -0400

    fix date generation for single digit day and month

--- generateRecentUpdates.js
+++ generateRecentUpdates.js
@@ -64,7 +64,7 @@ function getTitleFromFrontMatter(file) {
 }
 
 function getDateFromFileName(file) {
-  const match = file.match(/^(\d{4}-\d{2}-\d{2})-/);
+  const match = file.match(/^(\d{4}-\d{1,2}-\d{1,2})-/);
   if (match) {
     return match[1];
   }
