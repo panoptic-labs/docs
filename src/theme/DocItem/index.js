648e66462c9acf2e4863a902651e88aecf62d8a2
BrandonL20 <62954565+BrandonL20@users.noreply.github.com>
2024-08-02 16:49:34 -0600

    Added giscus comments to docs

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -3,6 +3,7 @@ import {HtmlClassNameProvider} from '@docusaurus/theme-common';
 import {DocProvider} from '@docusaurus/theme-common/internal';
 import DocItemMetadata from '@theme/DocItem/Metadata';
 import DocItemLayout from '@theme/DocItem/Layout';
+import GiscusComponent from '@site/src/components/GiscusComponent';
 export default function DocItem(props) {
   const docHtmlClassName = `docs-doc-id-${props.content.metadata.unversionedId}`;
   const MDXComponent = props.content;
@@ -12,6 +13,7 @@ export default function DocItem(props) {
         <DocItemMetadata />
         <DocItemLayout>
           <MDXComponent />
+          <GiscusComponent />
         </DocItemLayout>
       </HtmlClassNameProvider>
     </DocProvider>
